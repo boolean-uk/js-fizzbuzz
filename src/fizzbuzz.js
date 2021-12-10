21781daceb3ecc0870685c27dbba32c5c0daf642
Mike <mike.m.herron@googlemail.com>
2021-12-10 10:12:59 +0000

    Test PR action

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,7 +1,7 @@
 // TODO: Write your function in this file
 
-function test(a,b) {
-  return [a,a+1,b]
+function test (a, b) {
+  return [a, a + 1, b]
 }
 // TODO: Change undefined below to the name of your function
 module.exports = test
