21703a3838b0527e6efc23f1c89924b521e1cfc7
Paul Smart <pdsmart6@gmail.com>
2021-12-14 10:19:55 +0000

    Paul FizzBuzz Exercise

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,14 @@
 // TODO: Write your function in this file
+const fbFunc = function (lower,upper) {
+    let fbArray = [];
+    for (i=lower;i<=upper;i++){
+        let output = "";
+    if (i%3===0) {output += "Fizz";}
+    if (i%5===0) {output += "Buzz";}
 
+    if (output === "") {output=i;}
+    fbArray.push(output);
+    } return fbArray;
+}
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fbFunc
