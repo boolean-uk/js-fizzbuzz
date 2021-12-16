fe9f0481a3d8af8f7093ad2184937904fbe1e3c8
PointWolfAnj <anojan31@gmail.com>
2021-12-16 11:35:20 +0000

    final push

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,17 @@
 // TODO: Write your function in this file
+function FizzBuzz(lower, upper) {
+  const fizzBuzzArray = [];
+  for (let i = lower; i <= upper; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      fizzBuzzArray.push("FizzBuzz");
+    } else if (i % 5 === 0) {
+      fizzBuzzArray.push("Buzz");
+    } else if (i % 3 === 0) {
+      fizzBuzzArray.push("Fizz");
+    } else fizzBuzzArray.push(i);
+  }
+  return fizzBuzzArray;
+}
 
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = FizzBuzz;
