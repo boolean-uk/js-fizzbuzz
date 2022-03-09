3c1eec00dbecf0e73809c9bdad765eeda6de1142
Anica Rahman <anicarahman2009@yahoo.it>
2022-03-09 15:58:31 +0000

    Plan written down

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,14 @@
 // TODO: Write your function in this file
+function createFizzBuzz (lower, upper) {
+    const outputArray = []
+
+}
+
+// Function containing lower and upper should give us an array
+// Inside this array there should be:
+// 1. A string "Fizz" if a number is a multiple of 3 (3, 6, 9, 12 etc...)
+// 2. A string "Buzz" if a number id a multiple of 5 (5, 10, 15 etc...)
+// 3. A string "FizzBuzz" if a number can be multipled by both 3 and 5 (15, 30, etc...)
 
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = createFizzBuzz
