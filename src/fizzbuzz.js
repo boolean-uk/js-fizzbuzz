62c92350e38386f818469560a9ca009192e4fcaa
sigristarisa <arisasigrist.ch@gmail.com>
2022-03-09 16:26:32 +0100

    changed the name of the parameters

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,8 +1,8 @@
 // TODO: Write your function in this file
 
-const fizzBuzz = (numOne, numTwo) => {
+const fizzBuzz = (lower, upper) => {
     const answerArr = [];
-    for(let i = numOne; i <= numTwo; i++) {
+    for(let i = lower; i <= upper; i++) {
         if(i % 15 === 0) {
             answerArr.push("FizzBuzz")
         } else if (i % 3 === 0) {
