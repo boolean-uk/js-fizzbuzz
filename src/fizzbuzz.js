c4fdd93e3fb20061ec29db4f6d51de2e0373e80b
Gui Rodrigues <guirocrpe@gmail.com>
2022-03-09 17:05:18 +0100

    FizzBuzz passing tests

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,16 @@
 // TODO: Write your function in this file
+const fill = (lower, upper) => {
+  const arr = [];
+  for (let i = lower; i <= upper; i++) {
+    const three = i % 3 === 0;
+    const five = i % 5 === 0;
+    if (three && five) arr.push("FizzBuzz");
+    else if (three) arr.push("Fizz");
+    else if (five) arr.push("Buzz");
+    else arr.push(i);
+  }
 
+  return arr;
+};
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fill;
