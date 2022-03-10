054827cd78ee6f4ef78bd6e06ba4c359d9585030
Gui Rodrigues <guirocrpe@gmail.com>
2022-03-10 16:46:31 +0100

    Add class implementation

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,13 +1,12 @@
 // TODO: Write your function in this file
+
+const Element = require("./Element");
+
 const fill = (lower, upper) => {
   const arr = [];
   for (let i = lower; i <= upper; i++) {
-    const three = i % 3 === 0;
-    const five = i % 5 === 0;
-    if (three && five) arr.push("FizzBuzz");
-    else if (three) arr.push("Fizz");
-    else if (five) arr.push("Buzz");
-    else arr.push(i);
+    const element = new Element(i);
+    arr.push(element.output());
   }
 
   return arr;
