d1f1c71a0911c0694b6bce78b7889a64bf6759bf
gybgivee <kmcn0512@gmail.com>
2022-05-16 20:26:16 +0100

    complete

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,24 @@
 // TODO: Write your function in this file
+function fizzBuzz(lower,upper) {
+    let newArray=[];
+    let size=upper-lower;
+    size=size+1;
+    console.log(size);
+    
+   for (let i=0; i<size; i++){
+        newArray[i]=lower;
+        lower++;
+        if(newArray[i]%3==0&&newArray[i]%5!=0){
+            newArray[i]='Fizz';
+        }else if(newArray[i]%5==0&&newArray[i]%3!=0){
+            newArray[i]='Buzz';
+        }else if(newArray[i]%5==0&&newArray[i]%3==0){
+            newArray[i]='FizzBuzz';
+        }
+console.log(i+":"+newArray[i]);
 
+   }
+   return newArray;
+}
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fizzBuzz;
