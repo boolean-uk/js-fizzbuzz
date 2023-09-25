ffecafe70dcb386584c37c9109749a63160adabe
Loza_TM <lozatmamo@gmail.com>
2023-09-26 00:56:10 +0200

    fixed

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,26 +1,28 @@
 // TODO: Write your function in this file
-
+function fizzBuzz(lower, upper) {
+  const array = []
 // TODO: Change undefined below to the name of your function
-function fizzBuzzArray(lower, upper) {
-  const result = []
-  for (let i = lower; i <= upper; i++) {
+  for (let i = lower; i <= upper; i++){
     if (i % 3 === 0 && i % 5 === 0) {
-      result.push("FizzBuzz")
-    } else if (i % 3 === 0) {
-      result.push("Fizz")
-    } else if (i % 5 === 0) {
-      result.push("Buzz")
-    } else {
-      result.push(i)
+      array.push("FizzBuzz")
+    }
+    else if (i % 3 === 0){
+      array.push("Fizz")
+    }
+    else if (i % 5 === 0){
+      array.push("Buzz")
+    }
+    else {
+      array.push(i)
     }
   }
-  return result
-}
+return array 
+console.log(array)}
 
 // Example usage:
-const lower = 1
-const upper = 15
-const resultArray = fizzBuzzArray(lower, upper)
-console.log(resultArray)
-
-module.exports = undefined
+module.exports = undefined // i expored this because it was failing giving me an error that fizzBuzz is not defined.
+module.exports = fizzBuzz
+const lowerValue = 1
+const upperValue = 15
+const output = fizzBuzz(lowerValue, upperValue)
+console.log(output)
