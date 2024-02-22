95bbeceee972ef209bc7cb9b32b6b91c561eb7e0
Will Baxter <bax.will@googlemail.com>
2024-02-22 18:16:35 +0000

    Done fizzbuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,28 @@
 // TODO: Write your function in this file
 
+function fizzbuzz(lower, upper){
+    
+
+        let i = lower
+        fizzyArr = []
+
+        while (i <= upper){
+        if (i % 3 === 0 && i % 5 === 0){
+            fizzyArr.push('FizzBuzz')
+        } else if (i % 3 === 0){
+            fizzyArr.push('Fizz')
+        } else if (i % 5 === 0){
+            fizzyArr.push('Buzz')
+        } else {
+            fizzyArr.push(i)
+        }
+        i++
+        
+    }
+    return fizzyArr
+}
+
+console.log(fizzbuzz(1, 100))
+
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fizzbuzz
