cdd4a0e05305c6059c3537b2fbc4fd3438117c0c
Your Name <your_email@address>
2024-02-28 11:06:53 +0000

    exercise passed

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,20 @@
 // TODO: Write your function in this file
+function fizzBuzz(lower, upper) {
+  const arr = []
+  for (let i = lower; i <= upper; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push('FizzBuzz')
+    } else if (i % 3 === 0) {
+      arr.push('Fizz')
+    } else if (i % 5 === 0) {
+      arr.push('Buzz')
+    } else {
+      arr.push(i)
+    }
+  }
+
+  return arr
+}
 
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fizzBuzz
