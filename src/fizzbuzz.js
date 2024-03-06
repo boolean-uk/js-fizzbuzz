9e7ee5be77c5abbf84b3d6169c45bf8e18924002
timsakande <timiakande64@gmail.com>
2024-03-06 23:34:47 +0000

    fizzbuzz done late

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,17 @@
-// TODO: Write your function in this file
+function fizzBuzz(lower, upper) {
+    const result = [];
+    for (let i = lower; i <= upper; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push('FizzBuzz');
+        } else if (i % 3 === 0) {
+            result.push('Fizz');
+        } else if (i % 5 === 0) {
+            result.push('Buzz');
+        } else {
+            result.push(i);
+        }
+    }
+    return result;
+}
 
-// TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fizzBuzz;
